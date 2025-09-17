1588f1003bba7241f2ca632cce74ebbebfb06d12
Erik Veland <erik.veland@news.com.au>
2025-09-17 15:39:45 +1000

    Refines button hover effect
    Removes unwanted transform effect on button hover and active states.
    
    Updates webpack configuration to use const instead of let for webpack variable.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-let webpack = require('vortex-api/bin/webpack').default;
+const webpack = require('vortex-api/bin/webpack').default;
 
 const config = webpack('theme-switcher', __dirname, 5);
 config.externals['./build/Release/fontmanager'] = './fontmanager';
